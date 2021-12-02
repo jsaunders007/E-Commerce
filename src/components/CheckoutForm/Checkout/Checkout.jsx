fb2a605d29b9747294c837c7eff686c2356b49d3
jsaunders007 <jsaunders007@yahoo.com>
2021-12-02 00:36:59 -0600

    added more stuff

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,26 +1,75 @@
 import React from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from "@material-ui/core";
+import {commerce} from "../../../lib/Commerce";
 import useStyles from "./styles";
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
 import AddressForm from "../AddressForm";
 import PaymentForm from "../PaymentForm";
 
 
 const steps = [ "Shipping address", "Payment details" ];
 
-const Checkout = () => {
+const Checkout = ({cart, order, onCaptureCheckout, error}) => {
     const [activeStep, setActiveStep] = useState(0);
+    const [checkoutToken, setCheckoutToken] = useState(null);
+    const [shippingData, setShippingData] = useState({});
     const classes = useStyles();
 
-    const Form = () => activeStep === 0 
-    ? <AddressForm />
-    : <PaymentForm />
+    
 
-    const Confirmation = () => (
-        <div>
-            Confirmation
+    useEffect(() => {
+        const generateToken = async () => {
+            try {
+                const token = await commerce.checkout.generateToken(cart.id, { type: "cart"}) 
+                
+                console.log(token);
+                setCheckoutToken(token);
+            } catch (error) {
+                throw error
+            }
+        }
+        generateToken();
+    }, [cart]);
+
+    const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+
+    const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+    const next = (data) => {
+        setShippingData(data);
+        nextStep();
+    }
+
+    let Confirmation = () => order.customer ? (
+       <>
+       <div>
+           <Typography variant="h5">Thank you for your purchase, firstName lastName</Typography>
+           <Divider className={classes.divider} />
+           <Typography variant="subtitle2">Order ref: ref</Typography>
+       </div>
+       <br />
+       <Button component={Link} to="/" variant="outlined" type="button">Back to Home</Button>
+       </>
+    ) : (
+        <div className={classes.spinner}>
+            <CircularProgress />
         </div>
     )
+
+    if (error) {
+        <>
+        <Typography variant="h5">Error: {error}</Typography>
+        <br />
+        <Button component={Link} to="/" variant="outlined" type="button">Back to Home</Button>
+        </>
+    }
+
+    const Form = () => activeStep === 0 
+    ? <AddressForm checkoutToken={checkoutToken} next={next}/>
+    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} onCaptureCheckout={onCaptureCheckout}/>
+
+
     return (
     <>
         <div className={classes.toolbar}/> 
@@ -34,7 +83,7 @@ const Checkout = () => {
                         </Step>
                     ))}
                 </Stepper>
-                {activeStep === steps.length ? <Confirmation /> : <Form />}
+                {activeStep === steps.length ? <Confirmation /> : checkoutToken &&<Form />}
             </Paper>
             </main>   
         </>
--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { TextField, Grid } from "@material-ui/core";
 import { useFormContext, Controller} from "react-hook-form";
 
-const FormInput = ({name, label, required}) => {
+const FormInput = ({name, label}) => {
     const { control } = useFormContext();
     return (
         <Grid item xs={12} sm={6}>
             <Controller 
-            control={control}
-            name={name}
             render = {({field}) => (
                 <TextField 
+                defaultValue=""
+                control={control}
+                name={name}
                 fullwidth
             label={label}
-            required={required}
+            required
                 
                 />
             )}
